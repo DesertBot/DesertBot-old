1046ce17d31dfd1f4f03308f3939cea4fa0eda2b
Heufneutje <stefan.frijters@hotmail.com>
2014-06-04 07:53:45 +0000

    Check for required config values as proposed in #6

--- desertbot/config.py
+++ desertbot/config.py
@@ -6,10 +6,13 @@ class Config(object):
     def __init__(self, server):
         self.configData = {}
         yaml.load(os.path.join("configs", "{}.yaml".format(server)))
-        # TODO: Check whether or not all required config options are preesent
 
     def __iter__(self):
         return iter(self.configData)
 
     def __getitem__(self, key):
         return self.configData[key]
+
+    def checkRequiredValues(self):
+        required = ["nickname", "username", "realname"]
+        return True if required in self.configData else False
