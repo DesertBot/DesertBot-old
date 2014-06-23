4f1c8e72630b4252bcd80211273e8527f6cd9c37
Stefan <Heufneutje@users.noreply.github.com>
2014-06-23 14:24:50 +0200

    Fix formatting
    Converting 2 space tabs to 4

--- desertbot/modulehandler.py
+++ desertbot/modulehandler.py
@@ -6,37 +6,36 @@ from message import IRCMessage
 
 class ModuleHandler(object):
 
-  def __init__(self, bot):
-    """
-    @type bot: DesertBot
-    """
-    self.bot = bot
-    self.loadedModules = {}
+    def __init__(self, bot):
+        """
+        @type bot: DesertBot
+        """
+        self.bot = bot
+        self.loadedModules = {}
     
-  def sendResponse(self, response):
-    """
-    @type response: IRCResponse
-    """
-    pass
-    
-  def handleMessage(self, message):
-    """
-    @type message: IRCMessage
-    """
-    pass
+    def sendResponse(self, response):
+        """
+        @type response: IRCResponse
+        """
+        pass
+
+    def handleMessage(self, message):
+        """
+        @type message: IRCMessage
+        """
+        pass
   
-  def _loadModule(self, name):
-    """
-    @type name: unicode
-    """
-    pass
+    def _loadModule(self, name):
+        """
+        @type name: unicode
+        """
+        pass
     
-  def _unloadModule(self, name):
-    """
-    @type name: unicode
-    """
-    pass
+    def _unloadModule(self, name):
+        """
+        @type name: unicode
+        """
+        pass
     
-  def _loadAllModules(self):
-    pass
-    
+    def _loadAllModules(self):
+        pass
