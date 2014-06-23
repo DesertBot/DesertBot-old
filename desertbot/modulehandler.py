b7a0ff6b521ad9701baa11c8cffc4aaad14ac611
HubbeKing <hubbe128@gmail.com>
2014-06-23 15:19:49 +0300

    Create Module Handler skeleton
    Whoop whoop let's get started!

---
+++ desertbot/modulehandler.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from desertbot import DesertBot
+from response import IRCResponse
+from message import IRCMessage
+
+
+class ModuleHandler(object):
+
+  def __init__(self, bot):
+    """
+    @type bot: DesertBot
+    """
+    self.bot = bot
+    self.loadedModules = {}
+    
+  def sendResponse(self, response):
+    """
+    @type response: IRCResponse
+    """
+    pass
+    
+  def handleMessage(self, message):
+    """
+    @type message: IRCMessage
+    """
+    pass
+  
+  def _loadModule(self, name):
+    """
+    @type name: unicode
+    """
+    pass
+    
+  def _unloadModule(self, name):
+    """
+    @type name: unicode
+    """
+    pass
+    
+  def _loadAllModules(self):
+    pass
+    
