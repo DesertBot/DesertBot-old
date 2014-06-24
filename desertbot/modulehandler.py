8c3d75e64217c84885782495566fc473c0a0d9d4
HubbeKing <hubbe128@gmail.com>
2014-06-24 14:50:08 +0300

    Underp the indentation in unloadModule

--- desertbot/modulehandler.py
+++ desertbot/modulehandler.py
@@ -143,8 +143,8 @@ class ModuleHandler(object):
             try:
                 self.loadedModules[name.lower()].onModuleUnloaded()
             except Exception as e:
-                    errorMsg = "An error occurred while unloading module \"{}\" ({})".format(module.name, e)
-                    log.err(errorMsg)
+                errorMsg = "An error occurred while unloading module \"{}\" ({})".format(module.name, e)
+                log.err(errorMsg)
             # Unload module so it doesn't get stuck, but emit the error still.
             del self.loadedModules[name.lower()]
             loadMsg = "Module \"{}\" was successfully unloaded!".format(module.name)
