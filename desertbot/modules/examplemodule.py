f7d264cb911f9a8f0299f58bdeeb223729524890
HubbeKing <hubbe128@gmail.com>
2014-06-24 13:37:00 +0300

    Create examplemodule.py

---
+++ desertbot/modules/examplemodule.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from zope.interface import implements
+from twisted.plugin import IPlugin
+from moduleinterface import IModule, Module
+
+
+class ExampleModule(Module):
+    implements(IPlugin, IModule)
+    
+    name = u"examplemodule"
+    messageTypes = [u"PRIVMSG"]
+    helpText = "This example module outputs PRIVMSG-es to console!"
+    
+    def onTrigger(self, message):
+        print message.messageText
+        
