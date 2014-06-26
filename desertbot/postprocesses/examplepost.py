2aef859ce342b9ed09a39b0c7bc8e9ec4aeaf524
HubbeKing <hubbe128@gmail.com>
2014-06-26 14:37:24 +0300

    Create a sample postprocess module

---
+++ desertbot/postprocesses/examplepost.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from zope.interface import implements
+from twisted.plugin import IPlugin
+from desertbot.postprocessinterface import IPost, Module
+from desertbot.response import IRCResponse, ResponseType
+
+
+class ExamplePost(Module):
+    implements(IPlugin, IPost)
+    
+    name = u"examplepostprocess"
+    helpText = u"Appends responses with sillyness."
+    
+    def onTrigger(self, response):
+        """
+        @type response: IRCResponse
+        """
+        return IRCResponse(response.responseType, u"{} - Sent from DesertBot".format(response.response), response.user, response.target)
+
+examplepost = ExamplePost()
