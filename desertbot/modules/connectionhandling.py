b6d176b6bd6329d2095158677eebdd2a947f1a6a
HubbeKing <hubbe128@gmail.com>
2014-06-30 10:33:39 +0300

    Flesh out connect command

--- desertbot/modules/connectionhandling.py
+++ desertbot/modules/connectionhandling.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from zope.interface import implements
 from twisted.plugin import IPlugin
+from twisted.python import log
 
 from desertbot.moduleinterface import IModule, Module, ModuleType, AccessLevel
 from desertbot.message import IRCMessage
+from desertbot.response import IRCResponse, ResponseType
+from desertbot.config import Config
 
 
 class ConnectionHandling(Module):
@@ -13,7 +16,7 @@ class ConnectionHandling(Module):
     triggers = [u"connect", u"quit", u"quitfrom", u"restart", u"shutdown"]
     moduleType = ModuleType.COMMAND
     accessLevel = AccessLevel.ADMINS
-    helpText = u"connect <server> <channel> / quit / quitfrom <server> / restart / shutdown - " \
+    helpText = u"connect <server> [server] / quit / quitfrom <server> / restart / shutdown - " \
                u"handle bot connections"
 
     def onTrigger(self, message):
@@ -21,7 +24,27 @@ class ConnectionHandling(Module):
         @type message: IRCMessage
         """
         if message.command.lower() == u"connect":
-            pass
+            if len(message.parameterList) == 0:
+                return IRCResponse(ResponseType.PRIVMSG, u"Connect where?", message.user,
+                                   message.ReplyTo)
+            for server in message.parameterList:
+                try:
+                    config = Config("{}.yaml".format(server))
+                    if config.loadConfig():
+                        self.bot.bothandler.configs[server] = config
+                        self.bot.bothandler.startBotFactory(config)
+                        return IRCResponse(ResponseType.PRIVMSG,
+                                           u"Connecting to \"{}\"...".format(server),
+                                           message.user, message.replyTo)
+                    else:
+                        return IRCResponse(ResponseType.PRIVMSG, 
+                                           u"Couldn't connect to \"{}\".".format(server),
+                                           message.user, message.replyTo)
+                except Exception as e:
+                    log.err("Failed to connect to \"{}\" ({})".format(server, e)
+                    return IRCResponse(ResponseType.PRIVMSG, 
+                                       u"Could not connect to \"{}\" ({})".format(server, e),
+                                       message.user, message.replyTo)
         if message.command.lower() == u"quit":
             pass
         if message.command.lower() == u"quitfrom":
