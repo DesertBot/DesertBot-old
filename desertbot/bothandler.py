b4dc894e71c46b5ca3f53794bb732098e20e7c48
HubbeKing <hubbe128@gmail.com>
2014-07-01 08:48:30 +0300

    Use config file name as key in botfactories dict
    This way we're at least always doing the same thing :I

--- desertbot/bothandler.py
+++ desertbot/bothandler.py
@@ -11,12 +11,12 @@ from desertbot.config import Config
 class BotHandler(object):
     def __init__(self, cmdArgs):
         self.configs = {}
-        for server in cmdArgs.servers:
-            config = Config("{}.yaml".format(server))
+        for configFileName in cmdArgs.servers:
+            config = Config("{}.yaml".format(configFileName))
             if config.loadConfig():
-                self.configs[server] = config
+                self.configs[configFileName] = config
         self.botfactories = {}
-        for server, configObject in self.configs.iteritems():
+        for configFileName, configObject in self.configs.iteritems():
             self.startBotFactory(configObject)
         reactor.run()
 
@@ -24,37 +24,37 @@ class BotHandler(object):
         """
         @type config: Config
         """
-        if config["server"] in self.botfactories:
+        if config.configFileName[:-5] in self.botfactories:
             # already on this server for some reason
             return False
         else:
             botfactory = DesertBotFactory(self, config)
             reactor.connectTCP(config["server"], config["port"], botfactory)
-            self.botfactories[config["server"]] = botfactory
+            self.botfactories[config.configFileName[:-5]] = botfactory
             return True
 
-    def stopBotFactory(self, server, quitMessage=None):
+    def stopBotFactory(self, configFileName, quitMessage=None):
         if quitMessage is None or not isinstance(quitMessage, unicode):
             self.quitMessage = u"FINE. I'LL GO.".encode("utf-8")
         else:
             self.quitMessage = quitMessage.encode("utf-8")
-        if server not in self.botfactories:
+        if configFileName not in self.botfactories:
             # Not on this server at all!
             return False
         else:
             try:
-                self.botfactories[server].shouldReconnect = False
-                self.botfactories[server].bot.quit(self.quitMessage)
-                self._unloadModules(self.botfactories[server])
+                self.botfactories[configFileName].shouldReconnect = False
+                self.botfactories[configFileName].bot.quit(self.quitMessage)
+                self._unloadModules(self.botfactories[configFileName])
             except:
                 # Bot is probably stuck mid-reconnection
-                self.botfactories[server].stopTrying()
-            self.unregisterFactory(server)
+                self.botfactories[configFileName].stopTrying()
+            self.unregisterFactory(configFileName)
             return True
 
-    def unregisterFactory(self, server):
-        if server in self.botfactories:
-            del self.botfactories[server]
+    def unregisterFactory(self, configFileName):
+        if configFileName in self.botfactories:
+            del self.botfactories[configFileName]
 
             if len(self.botfactories) == 0:
                 # no more open connections
