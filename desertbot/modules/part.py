a0d289fbd23cf45c66993bc6ce1f16d09f60e130
Matthew Cox <matthewcpcox@gmail.com>
2014-07-01 12:50:36 +0100

    Add Part module
    Mara had some other ideas we might want to do later (with Join, too):
    I guess the neatest solution would be to have a joined channel store with an "inviter" that also has the right to part the bot from the channel, alongside the admins of course

---
+++ desertbot/modules/part.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from zope.interface import implements
+from twisted.plugin import IPlugin
+from twisted.python import log
+
+from desertbot.moduleinterface import IModule, Module, ModuleType, AccessLevel
+from desertbot.message import IRCMessage
+from desertbot.response import IRCResponse, ResponseType
+from desertbot.config import Config
+
+class Part(Module):
+    implements(IPlugin, IModule)
+
+    name = u"part"
+    triggers = [u"part", u"partfrom"]
+    moduleType = ModuleType.COMMAND
+    accessLevel = AccessLevel.ADMINS
+    helpText = u"part [message] / partfrom <channel> [message]"
+
+    def onTrigger(self, message):
+        """
+        @type message: IRCMessage
+        """
+        if message.command == u"part":
+            partMessage = None
+            if len(message.parameterList) > 0:
+                partMessage = message.parameters
+                
+            self.bot.leave(message.replyTo, reason=partMessage)
+            
+        elif message.command == u"partfrom":
+            if len(message.parameterList) == 0:
+                return IRCResponse(ResponseType.Say,
+                                   u"You didn't give a channel for me to partfrom",
+                                   message.user, message.replyTo)
+                                   
+            channel = message.parameterList[0]
+            partMessage = None
+            if len(message.parameterList) > 1:
+                partMessage = u" ".join(message.parameterList[1:])
+                
+            self.bot.leave(channel, reason=partMessage)
+
+part = Part()
+        
