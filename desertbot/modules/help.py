16eb7c32a792112255f8e0af0a9e397677e71464
HubbeKing <hubbe128@gmail.com>
2014-07-02 10:23:50 +0300

    Create help module

---
+++ desertbot/modules/help.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from zope.interface import implements
+from twisted.plugin import IPlugin
+
+from desertbot.moduleinterface import IModule, Module, ModuleType
+from desertbot.message import IRCMessage
+from desertbot.response import IRCResponse, ResponseType
+
+
+class Help(Module):
+    implements(IPlugin, IModule)
+    
+    name = u"help"
+    triggers = [u"help"]
+    moduleType = ModuleType.COMMAND
+    
+    def onTrigger(self, message):
+        if len(message.parameterList) == 0:
+            return IRCResponse(ResponseType.PRIVMSG, 
+                               u"Loaded modules: {}".format(u", ".join(self.bot.moduleHandler.loadedModules.keys())),
+                               message.user, message.replyTo)
+        else:
+            if message.parameterList[0].lower() in self.bot.moduleHandler.loadedModules.keys():
+                return IRCResponse(ResponseType.PRIVMSG,
+                                   self.bot.moduleHandler.loadedModules[message.parameterList[0].lower()].getHelp(),
+                                   message.user, message.replyTo)
+            else:
+                return IRCResponse(ResponseType.PRIVMSG,
+                                   u"There is no module called \"{}\" currently loaded.".format(message.parameterList[0]),
+                                   message.user, message.replyTo)
+                                   
+help = Help()
