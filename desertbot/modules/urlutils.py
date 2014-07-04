24f6198c39ee51928c0c593815dc8acbaa190275
Heufneutje <stefan.frijters@hotmail.com>
2014-07-04 11:06:45 +0200

    [modules/urlutils] Fix indents

--- desertbot/modules/urlutils.py
+++ desertbot/modules/urlutils.py
@@ -29,72 +29,72 @@ class URLUtils(Module):
         return
 
 
-def fetchURL(url, extraHeaders=None):
-    headers = [( "User-agent", "Mozilla/5.0" )]
-    if extraHeaders:
-        for header in extraHeaders:
-            # For whatever reason headers are defined in different way in opener than they are in
-            # a normal urlopen
-            headers.append((header, extraHeaders[header]))
-    try:
-        opener = build_opener()
-        opener.addheaders = headers
-        response = opener.open(url)
-        responseHeaders = response.info().dict
-        pageType = responseHeaders["content-type"]
+    def fetchURL(self, url, extraHeaders=None):
+        headers = [( "User-agent", "Mozilla/5.0" )]
+        if extraHeaders:
+            for header in extraHeaders:
+                # For whatever reason headers are defined in different way in opener than they are in
+                # a normal urlopen
+                headers.append((header, extraHeaders[header]))
+        try:
+            opener = build_opener()
+            opener.addheaders = headers
+            response = opener.open(url)
+            responseHeaders = response.info().dict
+            pageType = responseHeaders["content-type"]
 
-        # Make sure we don't download any unwanted things
-        if re.match(
-                "^(text/.*|application/((rss|atom|rdf)\+)?xml(;.*)?|application/(.*)json(;.*)?)$",
-                pageType):
-            urlResponse = URLResponse(response.read(), urlparse(response.geturl()).hostname,
-                                      responseHeaders)
-            response.close()
-            return urlResponse
-        else:
-            response.close()
+            # Make sure we don't download any unwanted things
+            if re.match(
+                    "^(text/.*|application/((rss|atom|rdf)\+)?xml(;.*)?|application/(.*)json(;.*)?)$",
+                    pageType):
+                urlResponse = URLResponse(response.read(), urlparse(response.geturl()).hostname,
+                                          responseHeaders)
+                response.close()
+                return urlResponse
+            else:
+                response.close()
 
-    except URLError as e:
-        reason = None
-        if hasattr(e, "reason"):
-            reason = "We failed to reach the server, reason: {}".format(e.reason)
-        elif hasattr(e, "code"):
-            reason = "The server couldn't fulfill the request, code: {}".format(e.code)
-        log.err("ERROR: Fetch from \"{}\" failed: {}".format(url, reason))
+        except URLError as e:
+            reason = None
+            if hasattr(e, "reason"):
+                reason = "We failed to reach the server, reason: {}".format(e.reason)
+            elif hasattr(e, "code"):
+                reason = "The server couldn't fulfill the request, code: {}".format(e.code)
+            log.err("ERROR: Fetch from \"{}\" failed: {}".format(url, reason))
 
 
-def postURL(url, values, extraHeaders=None):
-    headers = {"User-agent": "Mozilla/5.0"}
-    if extraHeaders:
-        for header in extraHeaders:
-            headers[header] = extraHeaders[header]
+    def postURL(self, url, values, extraHeaders=None):
+        headers = {"User-agent": "Mozilla/5.0"}
+        if extraHeaders:
+            for header in extraHeaders:
+                headers[header] = extraHeaders[header]
 
-    data = urlencode(values)
+        data = urlencode(values)
 
-    try:
-        request = Request(url, data, headers)
-        response = urlopen(request)
-        responseHeaders = response.info().dict
-        pageType = responseHeaders["content-type"]
+        try:
+            request = Request(url, data, headers)
+            response = urlopen(request)
+            responseHeaders = response.info().dict
+            pageType = responseHeaders["content-type"]
 
-        # Make sure we don't download any unwanted things
-        if re.match(
-                '^(text/.*|application/((rss|atom|rdf)\+)?xml(;.*)?|application/(.*)json(;.*)?)$',
-                pageType):
-            urlResponse = URLResponse(response.read(), urlparse(response.geturl()).hostname,
-                                      responseHeaders)
-            response.close()
-            return urlResponse
-        else:
-            response.close()
+            # Make sure we don't download any unwanted things
+            if re.match(
+                    '^(text/.*|application/((rss|atom|rdf)\+)?xml(;.*)?|application/(.*)json(;.*)?)$',
+                    pageType):
+                urlResponse = URLResponse(response.read(), urlparse(response.geturl()).hostname,
+                                          responseHeaders)
+                response.close()
+                return urlResponse
+            else:
+                response.close()
 
-    except URLError as e:
-        reason = None
-        if hasattr(e, "reason"):
-            reason = "We failed to reach the server, reason: {}".format(e.reason)
-        elif hasattr(e, "code"):
-            reason = "The server couldn't fulfill the request, code: {}".format(e.code)
-        log.err("ERROR: Post to \"{}\" failed: {}".format(url, reason))
+        except URLError as e:
+            reason = None
+            if hasattr(e, "reason"):
+                reason = "We failed to reach the server, reason: {}".format(e.reason)
+            elif hasattr(e, "code"):
+                reason = "The server couldn't fulfill the request, code: {}".format(e.code)
+            log.err("ERROR: Post to \"{}\" failed: {}".format(url, reason))
 
 
 urlutils = URLUtils()
