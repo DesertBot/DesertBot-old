11c2c442d0d5f6802a901c6201599a1e27e1e728
HubbeKing <hubbe128@gmail.com>
2014-07-16 23:54:56 +0300

    [modules] allow subdirs in modules folder

--- desertbot/modules/__init__.py
+++ desertbot/modules/__init__.py
@@ -1,5 +1,8 @@
 from twisted.plugin import pluginPackagePaths
-
+import os
 
 __path__.extend(pluginPackagePaths(__name__))
+for dir, subdirs, files in os.walk(os.path.dirname(os.path.realpath(__file__))):
+    for subdir in subdirs:
+        __path__.append(os.path.join(dir, subdir)) # Also include all module subdirectories in the path
 __all__ = []
