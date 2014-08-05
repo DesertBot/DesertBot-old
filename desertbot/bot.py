181fc774a5b7c9be265c24ef55488ccaef00f50c
Heufneutje <stefan.frijters@hotmail.com>
2014-08-05 12:48:39 +0000

    Fill out the server connection function

--- desertbot/bot.py
+++ desertbot/bot.py
@@ -1,15 +1,31 @@
 import pydle
 
+from botconnection import DesertBotConnection
+
 
 class DesertBot(object):
     def __init__(self, cmdArgs):
         self.connections = {}
+        self.configs = {}
         self.cmdArgs = cmdArgs
+        self.pool = pydle.ClientPool()
 
-    def startConnection(self):
+        self._loadConfigs()
+
+        self.pool.handle_forever()
+
+    def _loadConfigs(self):
         pass
 
-    def stopConnection(self):
+    def startConnection(self, server):
+        config = self.configs[server]
+        connection = DesertBotConnection(config["nicknames"][0], config["nicknames"][1:], username = config["username"], realname = config["realname"])
+
+        connection.connect(config["server"], config["port"], tls=False, tls_verify=False)
+        self.connections[config["server"]] = connection
+        self.pool.add(connection)
+
+    def stopConnection(self, server):
         pass
 
     def quit(self, restart = False):
