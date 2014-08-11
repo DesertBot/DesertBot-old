c48d3a82f31e98b8d7c8a0a16adb26aa952e4608
Heufneutje <stefan.frijters@gmail.com>
2014-08-11 14:56:43 +0200

    Make the log file and log level configurable

--- start.py
+++ start.py
@@ -7,6 +7,10 @@ from desertbot.bot import DesertBot
 
 parser = argparse.ArgumentParser(description="A modular IRC bot written in Python, using Pydle as "
                                              "its backend.")
+parser.add_argument("-f", "--logfile", help="The file the debug log will be written to (default "
+                                            "desertbot.log).", type=str, default="desertbot.log")
+parser.add_argument("-l", "--loglevel", help="The logging level that will be used while running "
+                                             "the bot (default INFO).", type=str, default="INFO")
 cmdArgs = parser.parse_args()
 
 if __name__ == "__main__":
@@ -17,19 +21,25 @@ if __name__ == "__main__":
     # Initialize logging
     logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s] "
                                      " %(message)s")
-    logger = logging.getLogger("")
-    logger.setLevel(logging.INFO)
+    logger = logging.getLogger()
+
+    # Determine the logging level
+    numeric_level = getattr(logging, cmdArgs.loglevel.upper(), None)
+    if isinstance(numeric_level, int):
+        logger.setLevel(numeric_level)
+    else:
+        raise ValueError("Invalid log level {}".format(cmdArgs.loglevel))
 
     # Set up file logging
-    fileHandler = logging.FileHandler("desertbot.log")
+    fileHandler = logging.FileHandler(cmdArgs.logfile)
     fileHandler.setFormatter(logFormatter)
-    fileHandler.setLevel(logging.INFO)
+    fileHandler.setLevel(numeric_level)
     logger.addHandler(fileHandler)
 
     # Set up console logging
     consoleHandler = logging.StreamHandler()
     consoleHandler.setFormatter(logFormatter)
-    consoleHandler.setLevel(logging.INFO)
+    consoleHandler.setLevel(numeric_level)
     logger.addHandler(consoleHandler)
 
     # Create the bot to get started
